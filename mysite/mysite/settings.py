16ce1afd30190ded4aae0872af313d855befa7c5
Ashley Medell <ashleymedell@mail.weber.edu>
2023-11-07 10:16:25 -0700

    in progress

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_Root = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_Root = os.path.join(BASE_DIR, 'images')
+MEDIA_URL = '/images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
